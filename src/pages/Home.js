7cce7d19a6a5ca918d68d3710def0660cc96bfe6
tranquyenanhmedia <157691804+tranquyenanhmedia@users.noreply.github.com>
2024-01-25 01:50:28 -0800

    check

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
-import React from 'react'
-// import React, { useEffect } from 'react'
+// import React from 'react'
+import React, { useEffect } from 'react'
 import GetInTouch from '../components/GetInTouch';
 import GetStarted from '../components/GetStarted';
 import Header from '../components/Header';
 import Navbar from './../components/Navbar';
 import Footer from './../components/Footer/Footer';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
+  useEffect(() => {
   
-  //   navigate('/*');
+    navigate('/*');
 
-  // }, [])
+  }, [])
   return (
 
     <>
